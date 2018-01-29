0de65be9552c464bdbf54cbb4feb837efc3ece6b
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-01-29 12:38:25 +0200

    add mongoose getter (#3)

--- test/test_addon.js
+++ test/test_addon.js
@@ -6,13 +6,14 @@ const Addon = require('../src/addon');
 
 describe('addon', function () {
   it('can parse args', function () {
-    const addon = new Addon(1, 2, 3, 4, 5, 6);
+    const addon = new Addon(1, 2, 3, 4, 5, 6, 7);
     expect(addon.app).to.be.equal(1);
     expect(addon.server).to.be.equal(2);
     expect(addon.io).to.be.equal(3);
     expect(addon.eventBus).to.be.equal(4);
     expect(addon.logger).to.be.equal(5);
-    expect(addon.name).to.be.equal(6);
+    expect(addon.settings).to.be.equal(6);
+    expect(addon.mongoose).to.be.equal(7);
   });
   it('can parse args from object', function () {
     const options = {
@@ -21,7 +22,8 @@ describe('addon', function () {
       io: 3,
       eventBus: 4,
       logger: 5,
-      name: 6
+      settings: 6,
+      mongoose: 7
     };
     const addon = new Addon(options);
     expect(addon.app).to.be.equal(1);
@@ -29,6 +31,7 @@ describe('addon', function () {
     expect(addon.io).to.be.equal(3);
     expect(addon.eventBus).to.be.equal(4);
     expect(addon.logger).to.be.equal(5);
-    expect(addon.name).to.be.equal(6);
+    expect(addon.settings).to.be.equal(6);
+    expect(addon.mongoose).to.be.equal(7);
   });
 });
